11ab94810b7c18ecb368b52460707a20180c712f
KO1231 <k.oka@kazutech.jp>
2025-01-10 23:18:29 +0900

    add: 13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const idtext = document.getElementById("text");
+  if(e.target.checked){
+    idtext.style.backgroundColor = "#ff0000";
+  }else{
+    idtext.style.backgroundColor = "#ffffff";
+  }
 }
